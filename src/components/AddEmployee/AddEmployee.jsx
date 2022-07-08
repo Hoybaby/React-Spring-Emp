7868c7ec7e6f70921979aaf59f97c480a7cf7cee
Hoybaby <mbartek436@gmail.com>
2022-07-08 12:01:39 -0400

    added the buttons that will have the functionaility later. just making the buttons look good on the hover

--- src/components/AddEmployee/AddEmployee.jsx
+++ src/components/AddEmployee/AddEmployee.jsx
@@ -13,6 +13,23 @@ const AddEmployee = () => {
                 <label className='block text-gray-600 text-sm font-normal'>First Name</label>
                 <input className ='h-8 w-96 border mt-2 px-2 py-2' type="text"/> 
             </div>
+            <div className='justify-center items-center h-14 w-full my-4'>
+                <label className='block text-gray-600 text-sm font-normal'>Last Name</label>
+                <input className ='h-8 w-96 border mt-2 px-2 py-2' type="text"/> 
+            </div>
+            <div className='justify-center items-center h-14 w-full my-4'>
+                <label className='block text-gray-600 text-sm font-normal'>Email</label>
+                <input className ='h-8 w-96 border mt-2 px-2 py-2' type="email"/> 
+            </div>
+            <div className='justify-center items-center h-14 w-full my-4 selection space-x-4'>
+                <button className='rounded text-white font-semibold bg-green-500 py-2 px-6 hover:bg-green-700'>
+                    Save
+                </button>
+                <button className='rounded text-white font-semibold bg-red-500 py-2 px-6 hover:bg-red-700'>
+                    Clear
+                </button>
+            </div>
+            
 
         </div>
     </div>
