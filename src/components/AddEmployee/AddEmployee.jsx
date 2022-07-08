ace2e0bf8ce06bd8b968c412358b8a4977b727bb
Hoybaby <mbartek436@gmail.com>
2022-07-08 11:34:35 -0400

    syling the forms here now so it can look good alter

--- src/components/AddEmployee/AddEmployee.jsx
+++ src/components/AddEmployee/AddEmployee.jsx
@@ -9,8 +9,9 @@ const AddEmployee = () => {
             <div className="font-thin text-2xl tracking-wider">
                 <h1>Add New Employee</h1>
             </div>
-            <div className='justify-center items-center h-14 w-full'>
-                <label >First Name</label>
+            <div className='justify-center items-center h-14 w-full my-4'>
+                <label className='block text-gray-600 text-sm font-normal'>First Name</label>
+                <input className ='h-8 w-96 border mt-2 px-2 py-2' type="text"/> 
             </div>
 
         </div>
